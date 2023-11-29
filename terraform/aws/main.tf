2ebdd773c8a92661935cd5cb20c4de8f91db6927
Nader Hadad Souza <naderhadadsouza@gmail.com>
2023-11-28 23:52:23 -0300

    prova teste destoy

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,5 +1,3 @@
-data "aws_caller_identity" "current" {}
-
 resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = "true"
--- terraform/aws/vars.tf
+++
@@ -1,7 +0,0 @@
-# variable "bucket_name" {
-#   default = "nhs-gs-aws-01"
-# }
-
-variable "website_endpoint" {
-  default = "true"
-}
